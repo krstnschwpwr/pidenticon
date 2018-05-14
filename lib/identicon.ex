dcd7ec918b6be2252ac00fb66fb5066dff5e5cc7
Kristine <kristine@7mind.de>
2018-05-14 19:54:41 +0200

    added filter grid func

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -4,17 +4,33 @@ defmodule Identicon do
     |> hash_input
     |> pick_color
     |> build_grid
+    |> filter_odd_squares
+  end
+
+  def filter_odd_squares(%Identicon.Image{grid: grid} = image) do
+    grid = Enum.filter grid, fn({code, _index}) ->
+      #calc remainder /2
+      rem(code, 2) == 0
+    end
+    %Identicon.Image{image | grid: grid}
+  end
+
+  def build_grid(%Identicon.Image{hex: hex} = image) do
+    grid =
+      hex
+      |> Enum.chunk(3)
+      #run it every row + args it takes
+      |> Enum.map(&mirror_row/1)
+      #create single list
+      |> List.flatten
+      #get index
+      |> Enum.with_index
+
+    %Identicon.Image{image | grid: grid}
   end
 
   def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
     %Identicon.Image{image | color: {r, g, b}}
-  end
-
-  def build_grid(%Identicon.Image{hex: hex = image}) do
-    hex
-    |> Enum.chunk(3)
-    #run it every row + args it takes
-    |> Enum.map(&mirror_row/1)
   end
 
   def mirror_row(row) do
